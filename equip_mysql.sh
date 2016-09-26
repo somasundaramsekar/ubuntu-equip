7d61b149aa032f66bdd691d51ac78f8b495aaa37
somasundaramsekar <somasundaram@outlook.com>
2016-09-26 20:57:10 +0530

    add equip mysql | password prompt less

--- equip_mysql.sh
+++ equip_mysql.sh
@@ -2,7 +2,10 @@
 
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
-sudo apt-get install -y mysql-server
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+sudo apt-get -y install mysql-server
 
 sudo service mysql start
 
