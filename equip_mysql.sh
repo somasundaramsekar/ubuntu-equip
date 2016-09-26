821322062b049a26558d4f3e8393eaf1231e283e
somasundaramsekar <somasundaram@outlook.com>
2016-09-26 20:36:07 +0530

    add equip mysql

---
+++ equip_mysql.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo apt-get install mysql-server
+
+sudo service mysql start
+
+sudo /usr/sbin/update-rc.d mysql defaults
+
